4cfea6fffe578b22c7b3ef19c47b77181919e56e
hao.dang <hao.dang@fidesio.com>
2019-11-27 23:18:25 +0700

    updated breadcrumb

--- local/resources/views/site/common/bread_crum.blade.php
+++ local/resources/views/site/common/bread_crum.blade.php
@@ -1,24 +1,16 @@
-<div class="bread-crumb">
-    <nav aria-label="breadcrumb">
-        @if(isset($_REQUEST['post_id']))
-            @php
-            $post_cat_id = getPostById($_REQUEST['post_id'])->post_category_id;
-            $post_cat_name = getPostCatById($post_cat_id)->post_cat_name;
-            @endphp
-            <ol class="breadcrumb" style="width: 30%">
-                <li class="breadcrumb-item"><a href="{{url('/')}}">Home</a></li>
-                <li class="breadcrumb-item"><a href="#">@php echo $post_cat_name @endphp</a></li>
-                <li class="breadcrumb-item active" aria-current="page">Bài Viết</li>
-            </ol>
-        @elseif(isset($_REQUEST['cat_id']))
-            <ol class="breadcrumb" style="width: 20%">
-                <li class="breadcrumb-item"><a href="{{url('/')}}">Home</a></li>
-                <li class="breadcrumb-item active" aria-current="page"><a href="#">{{getPostCatById($_REQUEST['cat_id'])->post_cat_name}}</a></li>
-            </ol>
-        @else
-            <ol class="breadcrumb" style="width: 10%">
-                <li class="breadcrumb-item active" aria-current="page"><a href="{{url('/')}}">Home</a></li>
-            </ol>
-        @endif
-    </nav>
-</div>
+<ul class="breadcrumb">
+    @if(isset($_REQUEST['post_id']))
+        @php
+        $post_cat_id = getPostById($_REQUEST['post_id'])->post_category_id;
+        $post_cat_name = getPostCatById($post_cat_id)->post_cat_name;
+        @endphp
+        <li><a href="#">Home</a></li>
+        <li><a href="#">@php echo $post_cat_name @endphp</a></li>
+        <li><a href="#">Bài Viết</a></li>
+    @elseif(isset($_REQUEST['cat_id']))
+        <li><a href="#">Home</a></li>
+        <li><a href="#">{{getPostCatById($_REQUEST['cat_id'])->post_cat_name}}</a></li>
+    @else
+        <li><a href="#">Home</a></li>
+    @endif
+</ul>
